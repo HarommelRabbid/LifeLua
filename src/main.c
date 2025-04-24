f7c6836329008801d7c771970dc852b850454b95
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-04-24 13:55:14 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -18,11 +18,49 @@
 
 lua_State *L;
 
+int lua_delay(lua_State *L) {
+	int argc = lua_gettop(L);
+	int secs;
+	if(argc<=0){
+		secs = 0;
+	}
+	else{
+		secs = luaL_checkinteger(L, 1);
+	}
+    sceKernelDelayThread(secs * 1000000); // this converts microsecs to secs
+    return 0;
+}
+int lua_exit(lua_State *L) {
+    sceKernelExitProcess(0);
+    return 0;
+}
+
+// "extending" the "os" module
+static const struct luaL_Reg os_lib[] = {
+    {"delay", lua_delay},
+    {"exit", lua_exit},
+    {NULL, NULL}
+};
+
+void luaL_openos(lua_State *L) {
+	lua_getglobal(L, "os"); // Get existing os table
+	if (!lua_istable(L, -1)) {
+		lua_pop(L, 1);        // Remove whatever was on the stack
+		lua_newtable(L);      // Create a new one if it doesn’t exist
+		lua_setglobal(L, "os");
+		lua_getglobal(L, "os");
+	}
+
+	luaL_setfuncs(L, os_lib, 0); // Add your functions to the os table
+	lua_pop(L, 1); // Pop the os table off the stack
+}
+
 int main(){
 	sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
 
 	L = luaL_newstate();
 	luaL_openlibs(L);
+	luaL_openos(L);
 
 	if (luaL_dofile(L, "app0:main.lua") != LUA_OK) {
 		psvDebugScreenInit();
