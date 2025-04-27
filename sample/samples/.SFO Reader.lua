492f4fa918fdf0802eb185e3af996f7cb4857ae3
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-04-27 23:48:56 +0300

    Update and rename Keyboard.lua to .SFO Reader.lua

--- sample/samples/.SFO Reader.lua
+++ sample/samples/.SFO Reader.lua
@@ -1,6 +1,7 @@
 sfo = io.readsfo("app0:sce_sys/param.sfo")
 while true do
-local y = 10
+draw.text(10, 10, "From 'app0:sce_sys/param.sfo': ", white)
+local y = 50
 for k, v in pairs(sfo) do
 draw.text(10, y, tostring(k)..": "..v, white)
 y = y + 20
