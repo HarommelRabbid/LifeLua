be36f58f1714958c8402f86dd3d5898d6ab7fe69
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-07-30 17:08:19 +0300

    Delete build directory

--- build/CMakeFiles/LifeLua-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LifeLua-velf"
-  "LifeLua.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LifeLua-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
