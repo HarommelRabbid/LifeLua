aa5d524a6a0bf2a8236d9842c61c291f947dbadf
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-09-04 15:03:42 +0300

    implementing libmpv

--- src/lj_lifevideo.c
+++ src/lj_lifevideo.c
@@ -18,204 +18,107 @@
 #include <vitasdk.h>
 #include <taihen.h>
 #include <vita2d.h>
-#include "include/vitaaudiolib.h"
+//#include "include/vitaaudiolib.h"
 
 #include "lj_lifeinit.h"
 
-#define FRAMEBUFFER_ALIGNMENT 0x40000
+#include <mpv/client.h>
+#include <mpv/render_gxm.h>
+#include <mpv/render.h>
 
-SceAvPlayerHandle avplayer;
-vita2d_texture *videobuf = NULL;
-SceAvPlayerFrameInfo video_frame;
-int videobuf_ref = LUA_NOREF;
+typedef struct {
+	vita2d_texture *img;
+	mpv_render_param mpv_params[3];
+	mpv_render_context *mpv_context;
+	mpv_handle *mpv;
+} Video;
+
 bool video_ready = false;
+static bool redraw = false;
 
-static int vaudio_thread(SceSize args, void* argp){
-	int ch = sceAudioOutOpenPort(SCE_AUDIO_OUT_PORT_TYPE_BGM, 1024, 48000, SCE_AUDIO_OUT_MODE_STEREO);
-	sceAudioOutSetConfig(ch, -1, -1, (SceAudioOutMode)-1);
-	
-	SceAvPlayerFrameInfo audio_frame;
-	memset(&audio_frame, 0, sizeof(SceAvPlayerFrameInfo));
-	
-	int vol_stereo[] = {SCE_AUDIO_OUT_MAX_VOL, SCE_AUDIO_OUT_MAX_VOL};
-	sceAudioOutSetVolume(ch, (SceAudioOutChannelFlag)(SCE_AUDIO_VOLUME_FLAG_L_CH | SCE_AUDIO_VOLUME_FLAG_R_CH), vol_stereo);
-	
-	while (video_ready) {
-		if (vol_stereo[0] != SCE_AUDIO_OUT_MAX_VOL) {
-			vol_stereo[0] = vol_stereo[1] = SCE_AUDIO_OUT_MAX_VOL;
-			sceAudioOutSetVolume(ch, (SceAudioOutChannelFlag)(SCE_AUDIO_VOLUME_FLAG_L_CH | SCE_AUDIO_VOLUME_FLAG_R_CH), vol_stereo);
-		}
-		if (sceAvPlayerIsActive(avplayer)) {
-			if (sceAvPlayerGetAudioData(avplayer, &audio_frame)) {
-				sceAudioOutSetConfig(ch, -1, audio_frame.details.audio.sampleRate, audio_frame.details.audio.channelCount == 1 ? SCE_AUDIO_OUT_MODE_MONO : SCE_AUDIO_OUT_MODE_STEREO);
-				sceAudioOutOutput(ch, audio_frame.pData);
-			} else {
-				sceKernelDelayThread(1000);
-			}
-		} else {
-			sceKernelDelayThread(1000);
-		}
-	}
-	
-	return sceKernelExitDeleteThread(0);
-}
-
-static void *memalloc(void *p, uint32_t alignment, uint32_t size)
-{
-    return memalign(alignment, size);
-}
-
-static void dealloc(void *p, void *ptr)
-{
-    free(ptr);
-}
-
-static void *gpu_alloc(void *p, uint32_t alignment, uint32_t size)
-{
-    void *res = NULL;
-
-    if (alignment < FRAMEBUFFER_ALIGNMENT)
-        alignment = FRAMEBUFFER_ALIGNMENT;
-
-    size = ALIGN(size, alignment);
-    SceKernelAllocMemBlockOpt opt;
-    memset(&opt, 0, sizeof(opt));
-    opt.size = sizeof(SceKernelAllocMemBlockOpt);
-    opt.attr = SCE_KERNEL_ALLOC_MEMBLOCK_ATTR_HAS_ALIGNMENT;
-    opt.alignment = alignment;
-    SceUID memblock = sceKernelAllocMemBlock("Video Memblock", SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW, size, &opt);
-    sceKernelGetMemBlockBase(memblock, &res);
-    sceGxmMapMemory(res, size, (SceGxmMemoryAttribFlags)(SCE_GXM_MEMORY_ATTRIB_READ | SCE_GXM_MEMORY_ATTRIB_WRITE));
-    return res;
-}
-
-static void gpu_dealloc(void *p, void *ptr)
-{
-    SceUID memblock = sceKernelFindMemBlockByAddr(ptr, 0);
-    sceGxmUnmapMemory(ptr);
-    sceKernelFreeMemBlock(memblock);
+static void mpv_render_update(void *cb_ctx) {
+    redraw = true;
 }
 
 static int lua_videoload(lua_State *L){
     if (video_ready) return 0;
+	Video *video = (Video *)lua_newuserdata(L, sizeof(Video));
 
 	const char *path = luaL_checkstring(L, 1);
-	bool loop = lua_toboolean(L, 3); // 2nd param is autostart
 
-	videobuf = (vita2d_texture *)malloc(sizeof(*videobuf));
+	video->mpv = mpv_create();
+    if (!video->mpv) {
+        printf("failed to create mpv context\n");
+        return EXIT_FAILURE;
+    }
 
-    SceAvPlayerInitData init_data;
-    memset(&init_data, 0, sizeof(SceAvPlayerInitData));
-    init_data.memoryReplacement.allocate = memalloc;
-    init_data.memoryReplacement.deallocate = dealloc;
-    init_data.memoryReplacement.allocateTexture = gpu_alloc;
-    init_data.memoryReplacement.deallocateTexture = gpu_dealloc;
-    init_data.basePriority = 125;
-    init_data.numOutputVideoFrameBuffers = 6;
-	init_data.debugLevel = 3;
-    init_data.autoStart = lua_toboolean(L, 2);
-    avplayer = sceAvPlayerInit(&init_data);
-	if(avplayer < 0) sceClibPrintf("Video init FAILED: 0x%X", avplayer);
+	mpv_set_option_string(video->mpv, "terminal", "yes");
+    mpv_set_option_string(video->mpv, "msg-level", "all=debug");
+    mpv_set_option_string(video->mpv, "vd-lavc-threads", "4");
+    mpv_set_option_string(video->mpv, "fbo-format", "rgba8");
+    mpv_set_option_string(video->mpv, "hwdec", "auto");
 
-	memset(&video_frame, 0, sizeof(SceAvPlayerFrameInfo));
+    // Disable direct rendering, only for testing, should be enabled in actual use
+    // mpv_set_option_string(mpv, "vd-lavc-dr", "no");
 
-    video_ready = true;
+    // Put font file to ux0:/data/fonts/ to test libass
+    mpv_set_option_string(video->mpv, "osd-fonts-dir", "ux0:/data/fonts");
+    mpv_set_option_string(video->mpv, "osd-font", "Open Sans");
+    mpv_set_option_string(video->mpv, "osd-msg1", "libass text");
 
-	int res = sceAvPlayerAddSource(avplayer, path);
-	if (res < 0) luaL_error(L, "sceAvPlayerAddSource failed: 0x%08X", res);
-	sceAvPlayerSetLooping(avplayer, loop);
-	sceAvPlayerSetTrickSpeed(avplayer, 100);
+    printf("Initialize mpv render context\n");
+    mpv_gxm_init_params gxm_params = {
+            .context = vita2d_get_context(),
+            .shader_patcher = vita2d_get_shader_patcher(),
+            .buffer_index = 0,
+            .msaa = SCE_GXM_MULTISAMPLE_NONE,
+    };
 
-	if(audio_active){
-		vitaAudioSetChannelCallback(0, NULL, NULL);
-		vitaAudioEndPre();
-		vitaAudioEnd();
-		audio_active = false;
-	}
-	SceUID audio_thread = sceKernelCreateThread("LifeLua Video Audio Thread", &vaudio_thread, 0x10000100, 0x10000, 0, 0, NULL);
-	sceKernelStartThread(audio_thread, 0, NULL);
-	audio_active = true;
+    mpv_render_param params[] = {
+            {MPV_RENDER_PARAM_API_TYPE,        (void *) MPV_RENDER_API_TYPE_GXM},
+            {MPV_RENDER_PARAM_GXM_INIT_PARAMS, &gxm_params},
+            {0}
+    };
+    if (mpv_render_context_create(&video->mpv_context, video->mpv, params) < 0) {
+        printf("failed to create mpv render context\n");
+        return EXIT_FAILURE;
+    }
+    printf("Set update callback\n");
+    mpv_render_context_set_update_callback(video->mpv_context, mpv_render_update, NULL);
+
+    printf("Initialize mpv\n");
+    if (mpv_initialize(video->mpv) < 0) {
+        printf("failed to initialize mpv\n");
+        return EXIT_FAILURE;
+    }
+
+	vita2d_texture *img = vita2d_create_empty_texture_rendertarget(960, 544, SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR);
+
+    int flip_y = 1;
+    mpv_gxm_fbo fbo = {
+            .render_target = img->gxm_rtgt,
+            .color_surface = &img->gxm_sfc,
+            .depth_stencil_surface = &img->gxm_sfd,
+            .w = 960,
+            .h = 544,
+            .format = SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_RGBA,
+    };
+    mpv_render_param mpv_params[3] = {
+            {MPV_RENDER_PARAM_FLIP_Y, &flip_y},
+            {MPV_RENDER_PARAM_GXM_FBO, &fbo},
+            {MPV_RENDER_PARAM_INVALID, NULL},
+    };
+
+    {
+        const char *cmd[] = {"loadfile", "file://ux0:/video/ik/Top Gear Parody.mp4", "replace", NULL};
+//        const char *cmd[] = {"loadfile", "file://ux0:/sintel_trailer-720p.mp4", NULL};
+        mpv_command(mpv, cmd);
+    }
     return 0;
-}
-
-static int lua_output(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	if(sceAvPlayerGetVideoData(avplayer, &video_frame)){
-		sceGxmTextureInitLinear(
-    	    &videobuf->gxm_tex,
-    	    video_frame.pData,
-    	    SCE_GXM_TEXTURE_FORMAT_YVU420P2_CSC1,
-    	    video_frame.details.video.width,
-    	    video_frame.details.video.height, 0);
-	}
-
-	if(videobuf != NULL){
-		float scale, startX;
-    	scale = 544.0f/(float)video_frame.details.video.height;
-    	startX = -((((float)video_frame.details.video.width*scale)-960.0f)/2);
-		vita2d_texture_set_filters(videobuf, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
-    	vita2d_draw_texture_scale(videobuf, startX, 0, scale, scale);
-	}
-	return 0;
-}
-
-static int lua_stop(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	sceAvPlayerStop(avplayer);
-	return 0;
-}
-
-static int lua_active(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	lua_pushboolean(L, sceAvPlayerIsActive(avplayer));
-	return 1;
-}
-
-static int lua_vpause(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	sceAvPlayerPause(avplayer);
-	return 0;
-}
-
-static int lua_vresume(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	sceAvPlayerResume(avplayer);
-	return 0;
-}
-
-static int lua_time(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	lua_pushnumber(L, sceAvPlayerCurrentTime(avplayer));
-	return 1;
-}
-
-static int lua_seek(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	uint64_t offset = luaL_checknumber(L, 1); // in seconds
-	sceAvPlayerJumpToTime(avplayer, offset*1000);
-	if(offset*1000 < sceAvPlayerCurrentTime(avplayer)) sceKernelDelayThread(100000);
-	return 0;
-}
-
-static int lua_videoclose(lua_State *L){
-	if(!video_ready) return luaL_error(L, "Video isn't initialized");
-	sceAvPlayerClose(avplayer);
-	vita2d_wait_rendering_done();
-	vita2d_free_texture(videobuf);
-	video_ready = false;
-	return 0;
 }
 
 static const luaL_Reg video_lib[] = {
     {"load", lua_videoload},
-    {"output", lua_output},
-	{"active", lua_active},
-	{"pause", lua_vpause},
-	{"resume", lua_vresume},
-	{"elapsed", lua_time},
-	{"seek", lua_seek},
-    {"stop", lua_stop},
-	{"close", lua_videoclose},
     {NULL, NULL}
 };
 
