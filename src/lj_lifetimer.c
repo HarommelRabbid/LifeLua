7a0e89cc746cabfe95d4bd948826d9ac5deb8cf6
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2025-09-12 23:19:43 +0300

    This was easier than I thought

--- src/lj_lifetimer.c
+++ src/lj_lifetimer.c
@@ -17,18 +17,38 @@
 #include <vitasdk.h>
 #include <taihen.h>
 #include <vita2d.h>
-#include "include/libtimer.h"
+//#include "include/libtimer.h"
 
 #include "lj_lifeinit.h"
 
 typedef struct {
-    Timer *timer;
-} lTimer;
+	uint32_t elapsed;
+	bool running;
+	uint32_t current_time;
+	uint32_t start_time;
+	uint32_t pause_time;
+	uint32_t pause_start;
+	uint32_t pause_end;
+} Timer;
+
+void updateTimer(Timer *timer){	
+	if(!timer->running) return;
+	
+	SceRtcTick rtick;
+	sceRtcGetCurrentTick(&rtick);
+	
+	timer->current_time = rtick.tick;
+	
+	timer->elapsed = timer->current_time - timer->start_time;
+}
 
 static int lua_newtimer(lua_State *L){
-    lTimer *timer = (lTimer *)lua_newuserdata(L, sizeof(lTimer));
+    Timer *timer = (Timer *)lua_newuserdata(L, sizeof(Timer));
 
-    timer->timer = createTimer();
+    SceRtcTick rtick;
+	sceRtcGetCurrentTick(&rtick);
+	
+	timer->start_time = rtick.tick;
     
     luaL_getmetatable(L, "timer");
     lua_setmetatable(L, -2);
@@ -36,61 +56,83 @@ static int lua_newtimer(lua_State *L){
 }
 
 static int lua_timerstart(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    startTimer(timer->timer);
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    SceRtcTick rtick;
+	sceRtcGetCurrentTick(&rtick);
+	
+	timer->start_time = rtick.tick;
+	timer->current_time = 0;
+	timer->running = true;
     return 0;
 }
 
 static int lua_timerpause(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    pauseTimer(timer->timer);
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    SceRtcTick rtick;
+	sceRtcGetCurrentTick(&rtick);
+	
+	if(timer->running) timer->pause_start = rtick.tick;
+	else {
+		timer->pause_end = rtick.tick;
+		timer->pause_time = timer->pause_end - timer->pause_start;
+		timer->start_time += timer->pause_time;
+	}
+	
+	timer->running = !timer->running;
     return 0;
 }
 
 static int lua_timerreset(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    resetTimer(timer->timer);
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    //this is how to reset a timer
+	timer->elapsed = 0;
+	timer->running = false;
+	timer->current_time = 0;
+	timer->pause_end = 0;
+	timer->pause_start = 0;
+	timer->pause_time = 0;
+	timer->start_time = 0;
     return 0;
 }
 
 static int lua_timerstop(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    stopTimer(timer->timer);
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    timer->running = false;
     return 0;
 }
 
 static int lua_timerelapsed(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    if (timer->timer->running) updateTimer(timer->timer);
-	lua_pushnumber(L, (double)(timer->timer->elapsed)/(1000*1000));
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    if (timer->running) updateTimer(timer);
+	lua_pushnumber(L, (double)(timer->elapsed)/(1000*1000));
     return 1;
 }
 
 static int lua_timerrunning(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
-    lua_pushboolean(L, timer->timer->running);
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
+    lua_pushboolean(L, timer->running);
     return 1;
 }
 
 static int lua_timerset(lua_State *L){
-    lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
+    Timer *timer = (Timer *)luaL_checkudata(L, 1, "timer");
     double seconds = luaL_checknumber(L, 2);
     SceRtcTick rtick;
 	sceRtcGetCurrentTick(&rtick);
 
 	uint64_t offset_ticks = (uint64_t)(seconds * 1000000.0); // seconds to microseconds
-	timer->timer->start_time = rtick.tick - offset_ticks;
-	timer->timer->current_time = rtick.tick;
-	timer->timer->elapsed = offset_ticks;
-	timer->timer->running = true;
+	timer->start_time = rtick.tick - offset_ticks;
+	timer->current_time = rtick.tick;
+	timer->elapsed = offset_ticks;
+	timer->running = true;
     return 0;
 }
 
-static int lua_timergc(lua_State *L){
+/*static int lua_timergc(lua_State *L){
     lTimer *timer = (lTimer *)luaL_checkudata(L, 1, "timer");
     freeTimer(timer->timer);
     return 1;
-}
+}*/
 
 static const luaL_Reg timer_lib[] = {
     {"new", lua_newtimer},
@@ -112,7 +154,7 @@ static const luaL_Reg timer_methods[] = {
     {"set", lua_timerset},
     {"elapsed", lua_timerelapsed},
     {"running", lua_timerrunning},
-    {"__gc", lua_timergc},
+    //{"__gc", lua_timergc},
     {NULL, NULL}
 };
 
